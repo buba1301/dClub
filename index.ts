e557e21567d08a54df669695c6ae9eac35b2d2f8
Boris Muzalevskii <bmuza57@gmail.com>
2021-09-02 14:42:24 +0300

    Add styles for body

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
+
 import App from './src/App';
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
