2d6174fc94e37c3fc974617091d20b3123169536
Boris Muzalevskii <bmuza57@gmail.com>
2021-09-02 15:39:02 +0300

    Add structure for Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,13 @@ import s from './Header.module.scss';
 const Header = () => (
   <div className={s.headerWrapper}>
     <div className={s.header}>
-      <div className={s.headerContainer} />
+      <div className={s.headerContainer}>
+        <div className={s.logoWrap}>1</div>
+        <div className={s.filtersContainer}>2</div>
+        <div className={s.seachContainer}>3</div>
+        <div className={s.buttonContainer}>4</div>
+        <div className={s.buscketContainer}>5</div>
+      </div>
     </div>
   </div>
 );
