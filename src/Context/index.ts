7c4438f6d75e5b1458c9274ba1fbd34ce45a74c5
Boris Muzalevskii <bmuza57@gmail.com>
2021-10-07 11:10:01 +0300

    Change context value type

--- src/Context/index.ts
+++ src/Context/index.ts
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import { Actions } from '../pages/Vendor';
 
-// import { ActiveFilters } from '../utils/filters';
-
-const FiltersContext = React.createContext<React.Dispatch<Actions>>(() => null);
+const FiltersContext = React.createContext<React.SetStateAction<[]>>([]);
 
 export default FiltersContext;
