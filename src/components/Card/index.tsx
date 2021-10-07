00f9d72e09be53bd7acfa07796ffc240da6a0dfd
Boris Muzalevskii <bmuza57@gmail.com>
2021-10-07 19:06:26 +0300

    Add styles for card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import cn from 'classnames';
 
 import s from './Card.module.scss';
 
@@ -9,7 +10,21 @@ type Props = {
 const Card = ({ name }: Props) => {
   const [] = React.useState();
 
-  return <div className={s.cards}>{name}</div>;
+  return (
+    <div className={s.cards}>
+      <div className={s.image} />
+      <div className={s.infoContainer}>
+        <div className={s.vendorName}>
+          <h3>{name}</h3>
+        </div>
+        <div className={s.specContainer}>
+          <div className={cn(s.badge, s.orderTime)}>1</div>
+          <div className={cn(s.badge, s.orderSum)}>2</div>
+          <div className={cn(s.badge, s.rating)}>3</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
